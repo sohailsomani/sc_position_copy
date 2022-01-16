227701fec47c96d64ab222da6b225540d8d7ab71
Sohail Somani <me@sohailsomani.com>
2022-01-16 14:27:17 -0500

    Add script to reload DLLs after building

--- src/primary/primary.cpp
+++ src/primary/primary.cpp
@@ -1,3 +1,15 @@
 #include "primary.hpp"
+#include "sierrachart.h"
 
-const char * hello_primary() {return "world";}
+const char *hello_primary() { return "world"; }
+
+SCDLLName("Position Copy Plugin for Primary Instance")
+
+SCSFExport scsf_TestingPrimary(SCStudyInterfaceRef sc) {
+  if (sc.SetDefaults) {
+    sc.AddMessageToLog("Testing, 123", 1);
+    sc.FreeDLL = 1;
+    sc.AutoLoop = 0;
+    return;
+  }
+}
