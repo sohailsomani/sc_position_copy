03df07ca54a310b738466f7a105d9c963547eee8
Sohail Somani <me@sohailsomani.com>
2022-01-17 00:45:18 -0500

    Move logic to plugin class

--- src/primary/primary.cpp
+++ src/primary/primary.cpp
@@ -1,5 +1,6 @@
 #include "primary.hpp"
 #include "boost/asio.hpp"
+#include "boost/asio/deadline_timer.hpp"
 #include "boost/asio/dispatch.hpp"
 #include "boost/asio/executor_work_guard.hpp"
 #include "boost/asio/io_service.hpp"
@@ -25,8 +26,7 @@ using boost::asio::io_service;
 
 struct Connection
 {
-  explicit Connection(io_service &service, tcp::socket socket)
-      : m_service(service), m_timer(m_service), m_socket(std::move(socket))
+  explicit Connection(tcp::socket socket) : m_socket(std::move(socket))
   {
     boost::system::error_code ec;
     auto endpoint = m_socket.remote_endpoint(ec);
@@ -39,50 +39,24 @@ struct Connection
       BOOST_LOG_TRIVIAL(error)
           << "Unable to get remote endpoint from socket! " << ec.message();
     }
-    sendPing();
   }
+
+  tcp::socket &socket() { return m_socket; }
 
 private:
-  void sendPing()
-  {
-    m_timer.expires_after(boost::asio::chrono::seconds(1));
-    m_timer.async_wait([this](const boost::system::error_code &ec) {
-      boost::asio::async_write(m_socket, boost::asio::buffer("PING"),
-                               [this](const boost::system::error_code &ec,
-                                      size_t bytes_transferred) {
-                                 if (processErrorCode(ec))
-                                   sendPing();
-                               });
-    });
-  }
-
-  bool processErrorCode(const boost::system::error_code &ec)
-  {
-    bool shouldKeepGoing = ec != boost::asio::error::eof &&
-                           ec != boost::asio::error::connection_reset;
-    if (!shouldKeepGoing)
-    {
-      boost::system::error_code ec;
-      auto endpoint = m_socket.remote_endpoint(ec);
-      BOOST_LOG_TRIVIAL(info) << "Connection lost: " << endpoint;
-    }
-    return shouldKeepGoing;
-  }
-
-  io_service &m_service;
-  boost::asio::steady_timer m_timer;
   tcp::socket m_socket;
 };
 
 struct PrimaryPlugin
 {
   explicit PrimaryPlugin(unsigned int port)
-      : m_port(port), m_work(m_service), m_endpoint(tcp::v4(), port),
-        m_acceptor(m_service, m_endpoint),
-        m_thread([this]() { this->threadFunc(); })
+      : m_position(0), m_port(port), m_work(m_service),
+        m_endpoint(tcp::v4(), port), m_acceptor(m_service, m_endpoint),
+        m_thread([this]() { this->threadFunc(); }), m_timer(m_service)
   {
     BOOST_LOG_TRIVIAL(info)
         << "Creating new primary server on port " << this->port();
+    sendPing();
   }
 
   ~PrimaryPlugin()
@@ -105,7 +79,54 @@ struct PrimaryPlugin
 
   unsigned int port() const { return m_port; }
 
+  void processPosition(t_OrderQuantity32_64 position)
+  {
+    if (position != m_position)
+    {
+      m_position = position;
+      sendPosition();
+    }
+  }
+
 private:
+  void sendPosition()
+  {
+    std::ostringstream oss;
+    oss << "{'position':" << m_position << "}";
+    std::string msg(oss.str());
+
+    for (auto &conn : m_connections)
+    {
+      if (conn->socket().is_open())
+      {
+        boost::asio::async_write(conn->socket(), boost::asio::buffer(msg),
+                                 m_completionHandler);
+      }
+    }
+  }
+
+  void sendPing()
+  {
+    auto end = std::remove_if(m_connections.begin(), m_connections.end(),
+                              [](std::unique_ptr<Connection> &conn) {
+                                return !conn->socket().is_open();
+                              });
+    m_connections.erase(end, m_connections.end());
+    auto tick = boost::posix_time::second_clock::local_time();
+    if (tick.time_of_day().seconds() % 5 == 0)
+    {
+      BOOST_LOG_TRIVIAL(info) << m_connections.size() << " clients connected";
+    }
+    for (auto &conn : m_connections)
+    {
+      boost::asio::async_write(conn->socket(), boost::asio::buffer("{}"),
+                               m_completionHandler);
+    }
+    m_timer.expires_after(boost::asio::chrono::seconds(1));
+    m_timer.async_wait(
+        [this](const boost::system::error_code &) { sendPing(); });
+  }
+
   void accept()
   {
     m_acceptor.async_accept(
@@ -113,8 +134,9 @@ private:
           if (!ec)
           {
             m_connections.push_back(
-                std::make_unique<Connection>(m_service, std::move(socket)));
+                std::make_unique<Connection>(std::move(socket)));
           }
+          sendPosition();
           accept();
         });
   }
@@ -143,6 +165,7 @@ private:
     BOOST_LOG_TRIVIAL(info) << "Thread done";
   }
 
+  t_OrderQuantity32_64 m_position;
   unsigned int m_port;
   boost::asio::io_service m_service;
   boost::asio::io_service::work m_work;
@@ -150,6 +173,10 @@ private:
   tcp::acceptor m_acceptor;
   std::vector<std::unique_ptr<Connection>> m_connections;
   std::thread m_thread;
+  boost::asio::steady_timer m_timer;
+  std::function<void(boost::system::error_code const &, std::size_t)>
+      m_completionHandler = [](boost::system::error_code const &ec,
+                               std::size_t bytesTransferred) {};
 };
 
 const char *hello_primary() { return "world"; }
@@ -196,5 +223,8 @@ SCSFExport scsf_PrimaryInstance(SCStudyInterfaceRef sc)
                .first;
       sc.AddMessageToLog("Started server", 0);
     }
+    s_SCPositionData position;
+    sc.GetTradePosition(position);
+    it->second->processPosition(position.PositionQuantity);
   }
 }
